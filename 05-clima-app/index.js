006639f06b42db0290f9d192d1e819479d5bd5e5
Adrián Hernández <hernandezsanchezluisadrian@gmail.com>
2023-10-11 13:39:21 -0600

    Node webserver railway

--- 05-clima-app/index.js
+++ 05-clima-app/index.js
@@ -16,18 +16,29 @@ const main = async()=>{
                 const lugares = await busquedas.ciudad(termino);
                  //Seleccionamos el lugar
                 const id = await listarLugares(lugares);
+                if(id === 0 ) continue;
                 const lugarSel = lugares.find(l => l.id === id);
-                // console.log(lugarSel);
+                //Guardar en DB 
+                busquedas.agregarHistorial(lugarSel.nombre)
                 //clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng)
 
                 //Mostrar resultados
+                console.clear();
                 console.log('\nInformacion de la ciudad\n'.green);
-                console.log('Ciudad:', lugarSel.nombre);
+                console.log('Ciudad:', lugarSel.nombre.green);
                 console.log('Lat:', lugarSel.lat);
                 console.log('Lng:', lugarSel.lng);
-                console.log('Temperatura:', );
-                console.log('Mínima:', );
-                console.log('Máxima', );
+                console.log('Temperatura:', clima.temp );
+                console.log('Mínima:', clima.min);
+                console.log('Máxima', clima.max);
+                console.log('Como esta el clima: ', clima.desc.green)
+            break;
+            case 2:
+                busquedas.historialCapitalizado.forEach((lugar, i)=>{
+                    const idx = `${i +1}.`.green;
+                    console.log(`${idx} ${lugar}`);
+                })
             break;
         }
         if (opt !==0) await pausa();
--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,11 +1,25 @@
+import fs from 'fs';
+
 import axios from 'axios';
 
 class Busquedas {
-    historial = ['Trgucigalpa', 'Madrid', 'San Jose'];
+    historial = [];
+    dbPath = './db/database.json';
 
     constructor() {
         //TODO: Leer BD si existe
+        this.leerBD();
     }
+
+    get historialCapitalizado(){
+        return this.historial.map(lugar=>{
+            let palabras = lugar.split(' ');
+
+            palabras = palabras.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return palabras.join(' ');
+        });
+    }  
 
     get paramsMapbox(){
         return {
@@ -13,6 +27,14 @@ class Busquedas {
             'access_token':process.env.MAPBOX_KEY,
             'limit': 5,
             
+        }
+    }
+
+    get paramsOpenWeather(){
+        return {
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
         }
     }
 
@@ -36,6 +58,58 @@ class Busquedas {
         } catch (error) {
             return [];
         }
+    }
+
+
+    async climaLugar(lat, lon){
+        try {
+            //Intacia de axios
+            const intance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: { ...this.paramsOpenWeather, lat, lon}
+
+            });
+            const resp = await intance.get();
+            const {weather, main} = resp.data;
+
+            return{
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async agregarHistorial(lugar = ''){
+        if(this.historial.includes(lugar.toLocaleLowerCase())){
+            return;
+        }
+
+        this.historial = this.historial.splice(0,5);
+        this.historial.unshift(lugar.toLocaleLowerCase());
+
+        //Guardar en DB
+        this.guardarDB()
+    }
+
+    guardarDB(){
+        const payload = {
+            historial: this.historial
+        };
+
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+    }
+
+    leerBD(){
+        if(!fs.existsSync(this.dbPath)) return;
+
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(info);
+
+        this.historial = data.historial;
     }
 }
 
